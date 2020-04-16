8b0aa0a4047a027e66314ee6f6877409c0e07b56
imhere <imhere@ih.com>
2020-04-16 17:30:25 +0800

    first commit

---
+++ db.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS mall.goods(
+    id SERIAL PRIMARY KEY,      
+    name VARCHAR(32) NOT NULL, 
+    intro VARCHAR(128), 
+    link TEXT,
+    price INTEGER DEFAULT 0, 
+    cover VARCHAR(32) NOT NULL, 
+    icon VARCHAR(32), 
+    swipe VARCHAR(32)[] NOT NULL, 
+    details VARCHAR(32)[], 
+    off BOOLEAN DEFAULT FALSE, 
+    recommend INTEGER DEFAULT 0, 
+    time TIMESTAMP WITH TIME ZONE DEFAULT LOCALTIMESTAMP(0)
+);
+CREATE INDEX IF NOT EXISTS goods_recommend_index ON mall.goods (recommend);
